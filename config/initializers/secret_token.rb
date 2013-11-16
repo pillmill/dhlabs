2861362e7f964e7765d3c0051f0b2baa8e3ffd01
Wes Vetter <wes.vetter@gmail.com>
2013-11-15 19:43:16 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Distributedhealth::Application.config.secret_key_base = 'b7d1ee9d4f80c2b7386871569f0c80d3667f0bde8f60280d82263ad886e6c69feca13732448bb6a9c829e96cfc3d031cef43bfe31048b0bf59e88656dbcc093f'
