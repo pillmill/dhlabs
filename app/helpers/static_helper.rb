b21069472cc884299f7a08d191c662ba1d6a8fc1
Wes Vetter <wes.vetter@gmail.com>
2013-11-28 21:33:36 -0800

    Better parallax (but not done)

--- app/helpers/static_helper.rb
+++ app/helpers/static_helper.rb
@@ -1,7 +1,7 @@
 module StaticHelper
-  def scroll_arrow(next_section)
+  def scroll_arrow(next_section, options={})
     arrow_icon = content_tag :i, '', class:'fa fa-angle-double-down fa-5x text-center'
     button = content_tag :button, arrow_icon, class:'btn btn-link', :data => {'next-section' => "#{next_section}" }, type:'button'
-    content_tag :div, button, class:'scroll-arrow'
+    content_tag :div, button, class:'scroll-arrow', id: options[:id]
   end
 end
