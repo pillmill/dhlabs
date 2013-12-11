a64c1223aa5a7a4979d0ebfd2614623fa6be1e69
Wes Vetter <wes.vetter@gmail.com>
2013-12-11 15:17:20 -0800

    Even MOAR responsive!!!

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,11 +23,12 @@ Distributedhealth::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  #config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
+  config.assets.compress
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  #config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
